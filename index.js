1c1fbff424e19d91a581dfd1f60a6f56b6c4f50f
Leigh Dotey <leighdotey@gmail.com>
2023-11-25 14:25:35 -0500

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
 
-// Write your code here
+let random = Math.ceil(Math.random()* 10)+2;
+
+let num3 = 24
+let num4 = 5
+let mod = num3 % num4;
+
+let numbers = (12,14,16,18,20)
+let max = Math.max(numbers)
